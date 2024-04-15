43e9f382493700df0ddaa03321e48c83f5aacfda
origin1508 <origin1508@gmail.com>
2024-04-15 23:28:40 +0900

    fix: users 데이터에 맞춰 수정

--- mocks/handlers/auth.ts
+++ mocks/handlers/auth.ts
@@ -1,9 +1,10 @@
 import { http, HttpResponse } from "msw";
-import { user } from "./data";
+import { users } from "./data";
 
 export const handler = [
   http.post("/auth/login/:provider", ({ params }) => {
     const { provider } = params;
+    const user = users[0];
 
     return HttpResponse.json({ success: true, result: { ...user, provider } });
   }),

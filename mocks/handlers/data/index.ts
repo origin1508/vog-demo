c3f402a5a0dc0844c53bea3400eaf943e7163241
origin1508 <origin1508@gmail.com>
2024-04-15 21:03:39 +0900

    feat: mock data 추가

---
+++ mocks/handlers/data/index.ts
@@ -0,0 +1,10 @@
+import profilePic from "./profile.jpg";
+
+export const user = {
+  oauthId: "test-oauth",
+  id: 1,
+  nickname: "테스트",
+  sex: "남",
+  jwtAccessToken: "test-access-token",
+  profileUrl: profilePic,
+};

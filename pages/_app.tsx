f9231cfae843d90871e6f11cd2bb1e3997830398
origin1508 <origin1508@gmail.com>
2024-04-15 17:37:05 +0900

    feat: msw 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,20 @@ import Socket from "@/components/Socket/Socket";
 import Loading from "@/components/common/Loading";
 
 export default function App({ Component, pageProps }: AppProps) {
+  if (process.env.NODE_ENV === "development") {
+    if (typeof window === "undefined") {
+      (async () => {
+        const { server } = await import("../mocks/server");
+        server.listen();
+      })();
+    } else {
+      (async () => {
+        const { worker } = await import("../mocks/browser");
+        worker.start();
+      })();
+    }
+  }
+
   return (
     <>
       <ErrorBoundary>

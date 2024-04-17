81a78a3a6183289504eb1941adfdedc358e98c93
origin1508 <origin1508@gmail.com>
2024-04-17 21:47:48 +0900

    feat: mock 데이터 추가

--- mocks/handlers/data/index.ts
+++ mocks/handlers/data/index.ts
@@ -9,4 +9,46 @@ export const users = [
     jwtAccessToken: "test-access-token",
     profileUrl: profilePic,
   },
+  {
+    oauthId: "test-oauth",
+    id: 1,
+    nickname: "테스트1",
+    sex: "남",
+    jwtAccessToken: "test-access-token",
+    profileUrl: profilePic,
+  },
+  {
+    oauthId: "test-oauth",
+    id: 2,
+    nickname: "테스트2",
+    sex: "남",
+    jwtAccessToken: "test-access-token",
+    profileUrl: profilePic,
+  },
+  {
+    oauthId: "test-oauth",
+    id: 3,
+    nickname: "테스트3",
+    sex: "남",
+    jwtAccessToken: "test-access-token",
+    profileUrl: profilePic,
+  },
 ];
+
+export const posts = {
+  free: [
+    {
+      id: 0,
+      writerId: 0,
+      title: "",
+      content: "",
+      view: 0,
+      likeCount: 0,
+      postCategory: "",
+      createAt: "",
+      updatedAt: "",
+    },
+  ],
+  humor: [],
+  championship: [],
+};

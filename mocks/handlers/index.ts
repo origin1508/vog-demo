987949fe3ece38771c4fb0876ba7c05ec081e6b6
origin1508 <origin1508@gmail.com>
2024-04-19 02:14:51 +0900

    fix: commentHandler 추가

--- mocks/handlers/index.ts
+++ mocks/handlers/index.ts
@@ -1,4 +1,13 @@
-import { handler as AuthHandler } from "./auth";
-import { handler as UserHandelr } from "./user";
+import { handler as authHandler } from "./auth";
+import { handler as userHandler } from "./user";
+import { handler as communityHandler } from "./community";
+import { handler as likeHandler } from "./like";
+import { handler as commentHandler } from "./comments";
 
-export const handlers = [...AuthHandler, ...UserHandelr];
+export const handlers = [
+  ...authHandler,
+  ...userHandler,
+  ...communityHandler,
+  ...likeHandler,
+  ...commentHandler,
+];

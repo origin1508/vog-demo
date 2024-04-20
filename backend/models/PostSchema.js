2bc19f5fbeec96b48531f9cd8d5b02b15b0d22b9
origin1508 <origin1508@gmail.com>
2024-04-20 16:53:46 +0900

    feat: PostSchema 작성

---
+++ backend/models/PostSchema.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const AutoIncrement = require("mongoose-sequence")(mongoose);
+
+const PostSchema = new Schema(
+  {
+    writerId: { type: Number, required: true },
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    postCategory: {
+      type: String,
+      required: true,
+      enum: ["free", "humor", "championship"],
+    },
+    view: { type: Number, default: 0 },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+    timestamps: true,
+  }
+);
+
+PostSchema.virtual("user", {
+  ref: "User",
+  localField: "writerId",
+  foreignField: "id",
+  justOne: true,
+});
+
+PostSchema.plugin(AutoIncrement, { id: "post_seq", inc_field: "id" });
+
+module.exports = mongoose.model("Post", PostSchema);

48539576979066ab2e8d77b5a6f581df84332e4a
origin1508 <origin1508@gmail.com>
2024-04-20 22:35:49 +0900

    feat: ChatSchema

---
+++ backend/models/ChatSchema.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { randomUUID } = require("crypto");
+
+const ChatSchema = new Schema(
+  {
+    roomId: { type: String, default: () => randomUUID() },
+    title: { type: String, required: true },
+    maximumMember: { type: Number, required: true, min: 2 },
+    currentMember: {
+      type: Number,
+      default: 0,
+      validate: {
+        validator: function (v) {
+          return v <= this.maximumMember;
+        },
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Chat", ChatSchema);

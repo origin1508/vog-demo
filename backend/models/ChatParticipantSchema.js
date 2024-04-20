3e1674d70742df1f5768e939985f50918aa4bd35
origin1508 <origin1508@gmail.com>
2024-04-21 01:34:24 +0900

    fix: roomId unique 옵션 제거

--- backend/models/ChatParticipantSchema.js
+++ backend/models/ChatParticipantSchema.js
@@ -5,11 +5,20 @@ const ChatParticipantSchema = new Schema(
   {
     userId: { type: Number, required: true, unique: true },
     socketId: { type: String, reqruied: true },
-    roomId: { type: String, required: true, unique: true },
+    roomId: { type: String, required: true },
   },
   {
     timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   }
 );
+
+ChatParticipantSchema.virtual("user", {
+  ref: "User",
+  localField: "userId",
+  foreignField: "id",
+  justOne: true,
+});
 
 module.exports = mongoose.model("ChatParticipant", ChatParticipantSchema);

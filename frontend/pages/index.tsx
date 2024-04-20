bcaadffeb1290f04f66f3dc5baa990757c49a0cd
origin1508 <origin1508@gmail.com>
2024-04-20 12:26:48 +0900

    fix: 로그인페이지로 돌아가는 에러 수정

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,21 +1,8 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import { useRecoilValue } from "recoil";
-import { loginState } from "@/recoil/selectors/loginState";
 import Home from "@/components/Home";
 import type { NextPageWithLayout } from "@/pages/_app";
 import MainLayout from "@/components/layout/MainLayout";
 
 const HomePage: NextPageWithLayout = () => {
-  const isLogin = useRecoilValue(loginState);
-  const router = useRouter();
-
-  useEffect(() => {
-    if (!isLogin) {
-      router.push("/login");
-    }
-  }, []);
-
   return <Home />;
 };
 

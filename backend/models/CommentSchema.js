8ec9ef6ee554caa68a7bda9bce2d12b9a13e78b9
origin1508 <origin1508@gmail.com>
2024-04-20 18:11:42 +0900

    feat: CommentSchema 작성

---
+++ backend/models/CommentSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const AutoIncrement = require("mongoose-sequence")(mongoose);
+
+const CommentSchema = new Schema(
+  {
+    writerId: { type: Number, required: true },
+    postId: { type: Number, required: true },
+    content: { type: String, required: true },
+  },
+  { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+CommentSchema.virtual("user", {
+  ref: "User",
+  localField: "writerId",
+  foreignField: "id",
+  justOne: true,
+});
+
+CommentSchema.plugin(AutoIncrement, { id: "comment_seq", inc_field: "id" });
+
+module.exports = mongoose.model("Comment", CommentSchema);

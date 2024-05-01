1ae86b5f700d9cd0a976e91d53253c03e0ee379e
origin1508 <origin1508@gmail.com>
2024-05-01 13:41:02 +0900

    fix: navigation과 버튼을 구분하기 위해 gap 적용

--- frontend/components/Sidebar/Sidebar.tsx
+++ frontend/components/Sidebar/Sidebar.tsx
@@ -36,27 +36,29 @@ const Sidebar = () => {
         {NAV_MENU.map((menu) => {
           const { name, href, icon } = menu;
           return (
-            <SidebarMenu key={name}>
+            <SidebarItem key={name}>
               <SidebarLink href={href}>
                 <ItemIcon>{icon}</ItemIcon>
                 {name}
               </SidebarLink>
-            </SidebarMenu>
+            </SidebarItem>
           );
         })}
-        <SidebarMenu>
-          <SidebarBtn onClick={handleFriendToggle}>
+      </SidebarNavigation>
+      <SidebarBtns>
+        <SidebarItem onClick={handleFriendToggle}>
+          <SidebarBtn>
             <ItemIcon>{getIcons("friends", 34)}</ItemIcon>
             친구목록
           </SidebarBtn>
-        </SidebarMenu>
-        <SidebarMenu>
-          <SidebarBtn onClick={handleLogout}>
+        </SidebarItem>
+        <SidebarItem onClick={handleLogout}>
+          <SidebarBtn>
             <ItemIcon>{getIcons("exit", 34)}</ItemIcon>
             로그아웃
           </SidebarBtn>
-        </SidebarMenu>
-      </SidebarNavigation>
+        </SidebarItem>
+      </SidebarBtns>
     </SidebarContainer>
   );
 };
@@ -64,7 +66,7 @@ const Sidebar = () => {
 export default Sidebar;
 
 const SidebarContainer = tw.nav`
-  flex flex-col w-80 h-full p-4 gap-5 border-r border-neutral-700 text-xl
+  flex flex-col w-80 h-full p-4 gap-8 border-r border-neutral-700 text-xl
 `;
 
 const SidebarLogo = tw.div`
@@ -79,7 +81,9 @@ const SidebarNavigation = tw.ul`
   font-semibold
 `;
 
-const SidebarMenu = tw.li`
+const SidebarBtns = tw.ul``;
+
+const SidebarItem = tw.li`
   h-12 py-2 px-4 rounded cursor-pointer
   hover:(bg-primary text-black)
 `;

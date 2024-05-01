1c539285d7ab8c13ecf8e933131cf9aeb6f848a1
origin1508 <origin1508@gmail.com>
2024-05-01 23:17:48 +0900

    style: noto sans 폰트 추가

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -9,6 +9,16 @@ export default function Document() {
           as="style"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/static/pretendard.css"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={""}
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <body>
         <Main />

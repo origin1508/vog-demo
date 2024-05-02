dad1918a66b133ebb78eb45235748f2b0e593df2
origin1508 <origin1508@gmail.com>
2024-05-02 19:24:52 +0900

    fix: 불필요한 코드 제거

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,13 +1,1 @@
-import Home from "@/components/Home";
-import type { NextPageWithLayout } from "@/pages/_app";
-import MainLayout from "@/components/layout/MainLayout";
-
-const HomePage: NextPageWithLayout = () => {
-  return <Home />;
-};
-
-HomePage.getLayout = function getLayout(page) {
-  return <MainLayout>{page}</MainLayout>;
-};
-
-export default HomePage;
+export { default } from "@/components/Home";

d26f645fdeebd116739a5007235dc9f97d3416bd
origin1508 <origin1508@gmail.com>
2024-05-06 14:00:03 +0900

    style: 아이콘 크기 수정

--- frontend/constants/nav.ts
+++ frontend/constants/nav.ts
@@ -1,23 +1,24 @@
 import { getIcons } from "@/components/icons";
 
 export const COMMUNITY_NAV_MENU = [
+  { name: "전체", query: undefined },
   { name: "자유게시판", query: "free" },
   { name: "유머게시판", query: "humor" },
   { name: "대회소식", query: "championship" },
 ];
 
 export const NAV_MENU = [
-  { name: "홈", pathname: "/", icon: getIcons("home", 34) },
+  { name: "홈", pathname: "/", icon: getIcons("home", 24) },
   {
     name: "커뮤니티",
     pathname: "/community",
     query: { category: "free" },
-    icon: getIcons("cardList", 34),
+    icon: getIcons("cardList", 24),
   },
-  { name: "채팅", pathname: "/chat", icon: getIcons("chat", 34) },
+  { name: "채팅", pathname: "/chat", icon: getIcons("chat", 24) },
   {
     name: "마이페이지",
     pathname: "/mypage",
-    icon: getIcons("avatar", 34),
+    icon: getIcons("avatar", 24),
   },
 ];
